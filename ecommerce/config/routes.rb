7a50efd13bf3e20ada50db31465be9e0d303eaeb
srijith kumar <srijith1312@gmail.com>
2023-09-13 14:35:20 +0530

    abc

--- ecommerce/config/routes.rb
+++ ecommerce/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :hotels do
+    resources :items
+  end
+
+  root 'hotels#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
